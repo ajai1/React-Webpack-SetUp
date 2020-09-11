c5b9f9118c68e7d79f590f5ac57cc826b9ca2cbc
ajai1 <emailtoajai@gmail.com>
2020-09-11 10:09:23 +0530

    Set-up Project

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className="app-header">Hi How are you?</h1>
+    </div>
+  );
+};
+
+export default App;
